359d73648a99145dac9d27f09f76b8a031b06c53
shabbir3g <mostafizshabbir@gmail.com>
2021-11-13 13:59:28 +0600

    delete product

--- index.js
+++ index.js
@@ -118,6 +118,13 @@ async function run(){
           res.send(products)
         }); 
 
+        app.delete('/products/:id', async(req, res) => {
+          const id = req.params.id;
+          const query = {_id: ObjectId(id)}
+          const result = await productsCollection.deleteOne(query);
+          res.json(result)
+        }); 
+
         app.get('/products/:id', async (req, res) => {
           const id = req.params.id;
           const query = {_id: ObjectId(id)};
